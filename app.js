f0cc2166692ba2c9c2c6bbb13d2926a784d7607d
lxhxr <cuchorapido@lxhxr.com>
2023-04-20 15:17:17 -0400

    primer commi

---
+++ app.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', cargarDatos);
+document.getElementById('postulacion-form').addEventListener('submit', agregarHabilidad);
+
+function cargarDatos() {
+    fetch('datos.json')
+        .then(response => response.json())
+        .then(datos => {
+            datos.forEach(dato => {
+                agregarHabilidadAlListado(dato.nombre, dato.habilidad, dato.contacto);
+            });
+        });
+}
+
+function agregarHabilidad(event) {
+    event.preventDefault();
+
+    const nombre = document.getElementById('nombre').value;
+    const habilidad = document.getElementById('habilidad').value;
+    const contacto = document.getElementById('contacto').value;
+
+    agregarHabilidadAlListado(nombre, habilidad, contacto);
+
+    document.getElementById('nombre').value = '';
+    document.getElementById('habilidad').value = '';
+    document.getElementById('contacto').value = '';
+}
+
+function agregarHabilidadAlListado(nombre, habilidad, contacto) {
+    const tbody = document.getElementById('listado-habilidades').querySelector('tbody');
+    const tr = document.createElement('tr');
+
+    const tdNombre = document.createElement('td');
+    tdNombre.textContent = nombre;
+    tr.appendChild(tdNombre);
+
+    const tdHabilidad = document.createElement('td');
+    tdHabilidad.textContent = habilidad;
+    tr.appendChild(tdHabilidad);
+
+    const tdContacto = document.createElement('td');
+    const aContacto = document.createElement('a');
+    aContacto.href = contacto;
+    aContacto.textContent = contacto;
+    aContacto.target = '_blank';
+    tdContacto.appendChild(aContacto);
+    tr.appendChild(tdContacto);
+
+    const tdEditar = document.createElement('td');
+    const btnEditar = document.createElement('button');
+    btnEditar.textContent = 'Editar';
+    btnEditar.addEventListener('click', () => editarHabilidad(tr));
+    tdEditar.appendChild(btnEditar);
+    tr
