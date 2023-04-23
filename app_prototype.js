3640a85128a803e951cd4590b990c7d5ac042de5
Dylan <72243034+PsylanL@users.noreply.github.com>
2023-04-23 15:18:23 -0500

    Propuesto de Prototipo Front (#5)
    * (FEAT) Added New Automatic Logo Selector & Divided project

--- app_prototype.js
+++ app_prototype.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', cargarDatos);
 
 function cargarDatos() {
-    fetch('datos.json')
+    fetch('datos_prototype.json')
         .then(response => response.json())
         .then(datos => {
             datos.forEach(dato => {
                 agregarHabilidadAlListado(dato.foto,dato.nombre, dato.habilidad, dato.contacto);
             });
         });
+        
 }
 
 function agregarHabilidad(event) {
@@ -29,38 +30,39 @@ function agregarHabilidad(event) {
 function agregarHabilidadAlListado(foto, nombre, habilidad, contacto) {
     const tbody = document.getElementById('listado-habilidades').querySelector('tbody');
     const tr = document.createElement('tr');
-
     const tdFoto = document.createElement('td')
     const img =document.createElement('img')
     img.setAttribute('src', foto)
     img.setAttribute('height', '150px')
-    tdFoto.appendChild(img);
+    tdFoto.appendChild(img)
     tr.appendChild(tdFoto)
+    // let logo = document.createElement("img");
+    // logo.src = dato.contacto;
 
-    const tdNombre = document.createElement('td');
-    tdNombre.textContent = nombre;
-    tr.appendChild(tdNombre);
+    // img.height = "64";
+    // img.width = "64";
+    tdFoto.appendChild(img);
+    const tdNombre = document.createElement('td')
+    tdNombre.textContent = nombre
+    tr.appendChild(tdNombre)
 
-    const tdHabilidad = document.createElement('td');
-    tdHabilidad.textContent = habilidad;
-    tr.appendChild(tdHabilidad);
+    const tdHabilidad = document.createElement('td')
+    tdHabilidad.textContent = habilidad
+    tr.appendChild(tdHabilidad)
 
-    const tdContacto = document.createElement('td');
+    const tdContacto = document.createElement('td')
     const aContacto = document.createElement('a');
     aContacto.href = contacto;
-    aContacto.textContent = 'Contacto';
     aContacto.target = '_blank';
-    tdContacto.appendChild(aContacto);
-    tr.appendChild(tdContacto);
-
-    // const tdEditar = document.createElement('td');
-    // const btnEditar = document.createElement('button');
-    // btnEditar.textContent = 'Editar';
-    // btnEditar.addEventListener('click', () => editarHabilidad(tr));
-    // tdEditar.appendChild(btnEditar);
-    // tr.appendChild(tdEditar);
-
-    tbody.appendChild(tr);
+    const imgContacto = document.createElement('img')
+    const contactoAnalizado = IdentificadorURL(contacto)
+    imgContacto.setAttribute('src', contactoAnalizado )
+    imgContacto.setAttribute('height', "100px")
+    
+    tdContacto.appendChild(aContacto)
+    aContacto.appendChild(imgContacto)
+    tr.appendChild(tdContacto)
+    tbody.appendChild(tr)
 }
 
 function editarHabilidad(tr) {
@@ -83,4 +85,41 @@ function editarHabilidad(tr) {
         tdContacto.contentEditable = 'false';
         tdEditar.textContent = 'Editar';
     }
+}
+
+function IdentificadorURL(contacto) {
+    let imgSrc = "";
+    
+     switch (true) {
+       case contacto.includes("instagram.com"):
+         redSocial = "Instagram";
+         imgSrc = "/imgs/Instagram-logo.webp";
+         break;
+       case contacto.includes("telegram.me") || contacto.includes("t.me"):
+         redSocial = "Telegram";
+         imgSrc = "/imgs/telegram-logo.webp";
+         break;
+       case contacto.includes("linkedin.com"):
+         redSocial = "LinkedIn";
+         imgSrc = "/imgs/linkedin-logo.webp";
+         break;
+       case contacto.includes("twitter.com"):
+         redSocial = "Twitter";
+         imgSrc = "/imgs/twitter-logo.webp";
+         break;
+       case contacto.includes("github.com"):
+         redSocial = "GitHub";
+         imgSrc = "/imgs/github-logo.webp";
+         break;
+       default:
+         redSocial = "Otro";
+         imgSrc = "/imgs/otros-logo.webp";
+         break;
+     }
+
+
+
+    // const contactoAnalizado = logo
+    return imgSrc
+
 }
