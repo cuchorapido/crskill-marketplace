2e9585a907e1eb8a383186741f584a18ce3c9370
Alvis <aherrera@munily.com>
2023-08-13 16:23:32 -0500

    feat: update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", onEventLoadCollaboratorHtml);
 const $cards = document.getElementById("cards_collaborators");
-const search = document.getElementById("searh_input").value;
 
 function cargarDatos() {
   fetch("datos_prototype.json")
@@ -163,12 +162,7 @@ function iconLinkedin(params) {
 
 function iconTwitter(params) {
   return `
-     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-    <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
-    <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
-    <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
-    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
     `;
 }
 
@@ -206,7 +200,7 @@ async function onEventLoadCollaboratorHtml() {
 
 function cardCollaborators({ nombre, habilidad, contacto }) {
   let icon = iconKeys.find((key) => contacto.includes(key.toLowerCase()));
-  if (!icon) icon = "link";
+  if (!icon) icon = "link"; 
   const iconSvg = objetIcons[icon]();
 
   return `
@@ -228,7 +222,9 @@ function cardCollaborators({ nombre, habilidad, contacto }) {
   `;
 }
 
-async function filterUsers() { 
+async function filterUsers() {
+  const search = document.getElementById("searh_input").value;
+
   const users = await getUsers();
   let html = "";
   const $cards = document.getElementById("cards_collaborators");
